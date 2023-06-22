4b27c3fdf2310ba1626afdf2a8ffe465dff75126
Your Name <you@example.com>
2023-06-22 22:26:47 +0530

    first commit

---
+++ src/routes/about/about.svelte
@@ -0,0 +1,36 @@
+<script>
+    // import Group from '../../Components/Group.svg';
+</script>
+  
+  <div class="background">
+    <!-- <Group/> -->
+    <img src="/Group.svg" alt="">
+    
+  </div>
+  
+  
+  
+  <style>
+    :global(body){
+          position: absolute;
+          background: #10013C;
+          height: 1089px;
+          width: 1440px;
+          left: 0px;
+          top: 1217px;
+          border-radius: 0px;
+  
+      }
+      .background {
+          position: relative;
+          height: 2284.4658203125px;
+  
+          position: absolute;
+          width: 1488px;
+        
+          bottom: 78.16%;
+  
+      }
+  
+  </style>
+  
